cc2a26a6ce0d05d92fc40bc2ad52f69392024234
PremePanther <shpadza@gmail.com>
2019-10-31 14:47:57 -0400

    added view changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import $ from "jquery";
 import "./App.css";
+
 import MovieRow from "./components/MovieRow";
 
 import logoSvg from "./moviedb-logo.svg";
@@ -8,7 +10,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      rows: null
+    };
 
     // const movies = [{}];
 
@@ -21,7 +25,36 @@ class App extends React.Component {
     this.handleSearch();
   }
 
-  handleSearch() {}
+  handleSearch() {
+    console.log("handleSearch running");
+    const apiUrl =
+      "https://api.themoviedb.org/3/search/movie?api_key=5f52126b52bf0626dd87fb729b84bfd0&language=en-US&query=marvel&page=1&include_adult=false";
+    //asic call to api for data
+    $.ajax({
+      url: apiUrl,
+      success: apiResults => {
+        console.log(`Got Data Successfully ${apiResults}`);
+        console.log(apiResults);
+
+        const results = apiResults.results;
+        console.log(results);
+
+        let movieRows = [];
+
+        results.forEach(movie => {
+          console.log(movie.title);
+          const movieList = <MovieRow key={movie.id} movie={movie} />;
+          movieRows.push(movieList);
+        });
+
+        this.setState({ rows: movieRows });
+      },
+      error: (xhr, status, err) => {
+        console.error("Failed to get data");
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -39,7 +72,7 @@ class App extends React.Component {
           </tbody>
         </table>
         <input className="searchBar" placeholder="Movie Search" />
-        {this.state}
+        {this.state.rows}
       </div>
     );
   }
